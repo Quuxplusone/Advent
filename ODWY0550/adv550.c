Quuxplusone/Advent

commit 85586f0bf5f48c6f0ffd9cb1559df93ea1ec94b2

Teach adv550 the "ignoreable" words from Platt's version.
This new code corresponds to line 164 in "PLAT0550/exec.f" and lines 33-34
in "PLAT0550/ADVENTURE.ACODE". Certain words, when they appear on the input
line, are simply ignored by the parser. This cleverly makes "GET IT" act the
same as "GET", for example. It's a very blunt instrument; you'll also find
that "THE A GET IT THEM LAMP" acts the same as "GET LAMP".

ODWY0550/adv550.c
@@ -850,6 +850,21 @@ void parse(struct InputWordInfo *w)
     }
 }
 
+bool is_ignoreable_word(const struct InputWordInfo *w)
+{
+    /* Certain words don't contribute to the input command at all. */
+    const char *s = w->text;
+    if (streq(s, "a")) return true;
+    if (streq(s, "an")) return true;
+    if (streq(s, "it")) return true;
+    if (streq(s, "that")) return true;
+    if (streq(s, "the")) return true;
+    if (streq(s, "them")) return true;
+    if (streq(s, "this")) return true;
+    if (streq(s, "to")) return true;
+    return false;
+}
+
 void listen(void)
 {
     static struct InputWordInfo saved_word;
@@ -878,25 +893,30 @@ void listen(void)
         printf("> "); fflush(stdout);
         fgets(buffer, sizeof buffer, stdin);
         for (p = buffer; isspace(*p); ++p) ;
-        if (*p == '\0') {
-            word1.text[0] = '\0';
-        } else {
-            for (q = word1.text; !isspace(*p) && *p != ','; ++p, ++q) {
-                if (q == word1.text+15) break;  /* buffer overflow prevention */
-                *q = tolower(*p);
+        do {
+            if (*p == '\0') {
+                word1.text[0] = '\0';
+            } else {
+                for (q = word1.text; !isspace(*p) && *p != ','; ++p, ++q) {
+                    if (q == word1.text+15) break;  /* buffer overflow prevention */
+                    *q = tolower(*p);
+                }
+                *q = '\0';
+                for (++p; isspace(*p); ++p) ;
             }
-            *q = '\0';
-            for (++p; isspace(*p); ++p) ;
-        }
-        if (*p == '\0') {
-            word2.text[0] = '\0';
-        } else {
-            for (q = word2.text; !isspace(*p) && *p != ','; ++p, ++q) {
-                if (q == word2.text+15) break;  /* buffer overflow prevention */
-                *q = tolower(*p);
+        } while (is_ignoreable_word(&word1));
+        do {
+            if (*p == '\0') {
+                word2.text[0] = '\0';
+            } else {
+                for (q = word2.text; !isspace(*p) && *p != ','; ++p, ++q) {
+                    if (q == word2.text+15) break;  /* buffer overflow prevention */
+                    *q = tolower(*p);
+                }
+                *q = '\0';
+                for (++p; isspace(*p); ++p) ;
             }
-            *q = '\0';
-        }
+        } while (is_ignoreable_word(&word2));
     }
 
     parse(&word1);

commit 1ac93f6efee582492257634f3a5608da93b02c94

Fix an ADV550 breakage introduced by me in revision 85586f0b.
Carelessness with boundary conditions caused "GET BIRD,ROD"
to break completely in that commit. I think this new code is
correct, although clearly I need to work on making it less
cryptic.

ODWY0550/adv550.c
@@ -882,6 +882,8 @@ void listen(void)
         fgets(buffer, sizeof buffer, stdin);
         for (p = buffer; isspace(*p); ++p) ;
         do {
+            if (*p == ',') ++p;
+            while (isspace(*p)) ++p;
             if (*p == '\0') {
                 word1.text[0] = '\0';
             } else {
@@ -890,10 +892,10 @@ void listen(void)
                     *q = tolower(*p);
                 }
                 *q = '\0';
-                for (++p; isspace(*p); ++p) ;
             }
         } while (is_ignoreable_word(&word1));
         do {
+            for (++p; isspace(*p); ++p) ;
             if (*p == '\0') {
                 word2.text[0] = '\0';
             } else {
@@ -902,7 +904,6 @@ void listen(void)
                     *q = tolower(*p);
                 }
                 *q = '\0';
-                for (++p; isspace(*p); ++p) ;
             }
         } while (is_ignoreable_word(&word2));
     }